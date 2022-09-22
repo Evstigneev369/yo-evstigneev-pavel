a6b98ce44e2466c2d115b3d00a04c60f1629fd5b
Павел <110538378+Evstigneev369@users.noreply.github.com>
2022-09-22 20:07:16 +0400

    дз
    попап

---
+++ js/index.js
@@ -0,0 +1,6 @@
+var popup = document.querySelector('.popup')
+var popupbtn = document.querySelector('.btn-js')
+
+popupbtn.addEventListener('click', function() {
+    popup.classList.add('popup-open ')
+})
