6a4e0e7bd66434179d2fc5fc09088ef7680e8ce7
Павел <110538378+Evstigneev369@users.noreply.github.com>
2022-09-23 14:21:19 +0400

    ДЗ
    esc - не работает

--- js/index.js
+++ js/index.js
@@ -1,11 +1,20 @@
 var popup = document.querySelector('.popup')
 var popupbtn = document.querySelector('.btn-js')
-var popupclose = document.querySelector('.popup-close')
+var popupclose = document.querySelector('.popup__cross')
+var input = popup.querySelector('input')
 
 popupbtn.addEventListener('click', function() {
-    popup.classList.add('popup-open') 
+    popup.classList.add('popup-open')
+    input.focus()
 })
 
 popupclose.addEventListener('click', function() {
     popup.classList.remove('popup-open')
+})
+
+window.addEventListener('keydown', function(event) {
+    if (event.code === "escape" && popup.classList.contains(popup-open)) {
+        popup.classList.remove('popup-open')
+        popupbtn.focus()
+    }
 })
