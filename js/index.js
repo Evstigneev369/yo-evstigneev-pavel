03cc109be097c755ba810ef7b92e813229bc13b9
Павел <110538378+Evstigneev369@users.noreply.github.com>
2022-09-23 12:47:04 +0400

    ДЗ
    Попап, проблема закрытие

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 var popup = document.querySelector('.popup')
 var popupbtn = document.querySelector('.btn-js')
+var popupclose = document.querySelector('.popup-close')
 
 popupbtn.addEventListener('click', function() {
-    popup.classList.add('popup-open ')
+    popup.classList.add('popup-open') 
+})
+
+popupclose.addEventListener('click', function() {
+    popup.classList.remove('popup-open')
 })
